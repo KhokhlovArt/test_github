e2e764c95dd7bcf062fb687b467df0e8d088baee
KhokhlovArt <KhokhlovArt@gmail.com>
2017-10-24 10:15:00 +0300

    ToDo list
    ToDo list

--- test_file.rb
+++ test_file.rb
@@ -2,9 +2,8 @@ Module.modify :UTH_WebContainer_Json2014Service do
 
   methods :self_configuire => %q{    ##{}
     def self.configure
-      start_1
-      start_2
-      start_3
+      #TODO:
+      # + 1
     end
 }
 end
