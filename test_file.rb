a9c143c5e5116e5c6c143c9779c41179e8f3d27c
KhokhlovArt <KhokhlovArt@gmail.com>
2017-10-26 11:06:15 +0300

    Set + 2
    Set + 2

--- test_file.rb
+++ test_file.rb
@@ -1,9 +1,10 @@
-Module.modify :UTH_WebContainer_Json2014Service do
+Module.modify :My_methods do
 
   methods :self_configuire => %q{    ##{}
     def self.configure
       #TODO:
       # + 1
+      # + 2 
     end
 }
 end
